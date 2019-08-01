6f9f4be389b1c9c5905241b174f046b942902ea7
Superdex517 <51976580+Superdex517@users.noreply.github.com>
2019-08-01 22:04:46 +0900

    sumin_0801_ver05
    UI, 하우스 작업

---
+++ Assets/02.woojin/01.[Scripts]/Billboard2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Billboard2 : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+}
