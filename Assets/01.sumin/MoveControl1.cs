413ab4b2e318433e261828327a0056f2a0595bbc
jinjin97 <dnwls8959@naver.com>
2019-08-02 15:45:55 +0900

    08.02최우진_VER04
    바이브 융합

---
+++ Assets/01.sumin/MoveControl1.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+public class MoveControl1 : MonoBehaviour
+{
+    SelectableUnitComponent selectableScript;
+    public NavMeshAgent nmAgent;
+    int wpCount = 0;
+    MoveManager mm;
+    Ray ray;
+    RaycastHit hit;
+    float maxDist = 100f;
+    LayerMask layerMaskTerrain;
+    void Start()
+    {
+        nmAgent = GetComponent<NavMeshAgent>();
+        selectableScript = GetComponent<SelectableUnitComponent>();
+        layerMaskTerrain = LayerMask.NameToLayer("TERRAIN");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && selectableScript.selectionCircle != null)
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, maxDist, 1 << layerMaskTerrain))
+            {
+                nmAgent.SetDestination(hit.point);
+                Debug.Log("Move");
+            }
+        }
+    }    
+    // 시뮬레이션 시작 버튼에서 처음 실행 
+    public void MoveArmy()
+    {
+        //if (nmAgent.isStopped)
+        nmAgent.SetDestination(LaserPointer.wpList[wpCount]);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("P : Enter Trigger");
+        if (other.CompareTag("WAYPOINT"))
+        {
+            Debug.Log("enter wp" + wpCount);
+            if (wpCount < LaserPointer.wpList.Count - 1)
+            {
+                //Debug.LogFormat("??? {0} < {1}" , wpCount, MoveManager.wpList.Count-1);
+                wpCount++;
+                MoveArmy();
+            }
+        }
+    }
+}
---
+++ Assets/MoveManager_.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Valve.VR;
+
+public class MoveManager_ : MonoBehaviour
+{
+
+    [Header("Controller Setup")]
+    public SteamVR_Input_Sources rightHand = SteamVR_Input_Sources.RightHand;
+
+    //트리거  버튼의 클릭 이벤트에 반응할 액션
+    public SteamVR_Action_Boolean trigger = SteamVR_Actions.default_InteractUI;
+
+    private RaycastHit hit;
+    private Ray ray = new Ray();
+    //컨트롤러의 transform 컴포넌트를 저장할 변수
+    private Transform tr;
+    public static bool isSettingWayPoint;
+    public GameObject wayPoint;
+    LayerMask layerMaskTerrain;
+    float maxDist = 100f;
+    public static List<Vector3> wpList;
+    public Canvas[] wpImage;
+    int wpImageCount;
+
+
+
+    NavMeshAgent[] navMeshAgents;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        wpList = new List<Vector3>();
+        layerMaskTerrain = LayerMask.NameToLayer("TERRAIN");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isSettingWayPoint)
+        {
+            Debug.Log("clicked for wp");
+            if (Physics.Raycast(tr.position, tr.forward, out hit, maxDist, 1 << layerMaskTerrain)
+                                                                && trigger.GetStateDown(rightHand))
+            {
+                Debug.Log("rayHit for wp");
+                wpList.Add(hit.point);
+                Instantiate(wayPoint, hit.point, Quaternion.identity);
+                Instantiate(wpImage[wpImageCount], (hit.point + new Vector3(0, 0.8f, 0)), Quaternion.identity);
+                wpImageCount++;
+
+            }
+        }
+
+    }
+    
+    
+    public void SetMakeWP()
+    {
+        isSettingWayPoint = !isSettingWayPoint;
+    }
+    public void StartSimulation()
+    {
+        navMeshAgents = FindObjectsOfType<NavMeshAgent>();
+        foreach (var nmAgent in navMeshAgents)
+        {
+            nmAgent.SendMessage("MoveArmy");
+        }
+    }
+}
